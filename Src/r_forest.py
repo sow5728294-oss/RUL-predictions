c7bcb5d256c42374ae0300a21543a6755777eacf
sow5728294 <sow5728294@gmail.com>
2026-09-05 17:22:17 +0800

    5:22

--- Src/r_forest.py
+++ Src/r_forest.py
@@ -1,2 +1,34 @@
 import pandas as pd
 from pathlib import Path
+
+#set base file location
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+#set specific processed data location
+processed_dir = BASE_DIR / "Data" / "target data" / "processed"
+
+
+#load the training data
+train_data = pd.read_csv(
+    processed_dir/"train_processed.csv"
+)
+
+#seperate into input and answer
+x_train = train_data.drop(columns=["unit", "RUL"])
+y_train = train_data["RUL"]
+
+
+#START CREATING MODEL
+
+from sklearn.ensemble import RandomForestRegressor
+
+#create the model
+model = RandomForestRegressor(
+    n_estimators = 100,
+    random_state = 42,
+    n_jobs = -1
+)
+
+#train the model
+
+model.fit(x_train,y_train)
